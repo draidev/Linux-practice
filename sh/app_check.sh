57e960d0ad20f61f7329684ba113b34b169308e1
jyjeon <jyjeon@localhost.localdomain>
2022-08-29 11:43:14 +0900

    modify app_check.sh

--- sh/app_check.sh
+++ sh/app_check.sh
@@ -7,54 +7,64 @@ SH=$0
 FILE=$1
 
 #./app_check -h : print usage and exit
-function usage(){
+function display_usage(){
 		echo "usage) $SH <list_file>"
 		exit 0
 }
 
 # ./app_check [FILE_NAME] : display process checked or missing
 function check_ps (){
+    arr=("$@")
 	if [[ -f "$1" ]]; then
-		i=0
+		i=1
 		while read line; do
 			ps_check=`ps aux --sort=-pmem | head -n 100 | grep $line`
 			if [[ "$ps_check" =~ "$line" ]]; then
-				echo "$line	checked"
+				echo "$line checked"
 			else
-				echo "$line	missing"
-				$2[i]=$line
-				((i+=1))
+                echo "$line missing"
+                miss_arr[i]="$line"
+                #arr[i]="$line"
+                #arr[i]="$line"
+                #echo "ARRAY ${#arr[@]}"
+                #echo "MISS ${#miss_arr[@]}"
+                ((i+=1)) 
 			fi
 		done < $1
 	# if input empty string
 	elif [[ ${#1} -eq 0 ]]; then
-		usage 
-	# if input empty file
+        echo "ERROR: input empty string"
+		display_usage
+
+	# if input not exist file
 	else
 		echo "ERROR: no such file '$1'"
-		usage	
-		exit 404
+		display_usage	
 	fi
 }
 
 function display_miss(){
-	for miss in $1
+    arr=$1
+    for miss in ${arr[*]}
 	do
 		echo "ERROR: '$miss' not found"
 	done
 }
 
 function display_success(){
-	if [[ $1 -ne 0 ]]&&[[ $2 -eq 0 ]]; then
 		echo "-----------------------------------------------------"
 		echo "All Success!"
-	fi
 }
 
+
 if [[ $FILE == $HELP ]]; then
-	usage
+	display_usage
 else
-	check_ps $FILE ${miss_arr[@]}
-	display_miss ${miss_arr[@]}
-	display_success ${#FILE} ${#miss_arr[@]}
+	check_ps $FILE "${miss_arr[@]}"
+	if [[ ${#miss_arr} -gt 0 ]]; then
+        display_miss "${miss_arr[*]}"
+    fi
+    if [[ ${#FILE} -gt 0 ]]&&[[ ${#miss_arr[@]} -eq 0 ]]; then
+	    display_success
+    fi
 fi
